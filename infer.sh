2201b49e1f4034d5460cc9478480fdbed023012e
Hayeong <hayeong028@gmail.com>
2024-02-01 09:05:09 +0900

    Update infer.sh

--- infer.sh
+++ infer.sh
@@ -2,7 +2,8 @@ python3 inference.py \
     --src_path './sample/src_p241_004.wav' \
     --trg_path './sample/tar_p239_022.wav' \
     --ckpt_model './ckpt/model_diffhier.pth' \
-    --ckpt_voc './vocoder/voc_ckpt.pth' \
+    --voc 'bigvgan' \
+    --ckpt_voc './vocoder/voc_bigvgan.pth' \
     --output_dir './converted' \
     --diffpitch_ts 30 \
     --diffvoice_ts 6
