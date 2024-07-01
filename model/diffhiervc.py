890c8b7888930a6641811cdd283a6a9dc0abfb1d
Hayeong <hayeong028@gmail.com>
2024-07-01 22:56:21 +0900

    Update diffhiervc.py

--- model/diffhiervc.py
+++ model/diffhiervc.py
@@ -13,8 +13,8 @@ import typing as tp
 
 from module.modules import *
 from module.utils import * 
- 
 
+ 
 class Wav2vec2(torch.nn.Module):
     def __init__(self, layer=12): 
         super().__init__() 
@@ -31,8 +31,7 @@ class Wav2vec2(torch.nn.Module):
         y = outputs.hidden_states[self.feature_layer]    
         
         return y.permute((0, 2, 1))    
-
-
+ 
 class Encoder(nn.Module):
     def __init__(self,
                  in_channels,
@@ -65,20 +64,20 @@ class Encoder(nn.Module):
 
 
 class SynthesizerTrn(nn.Module):
-    def __init__(self, encoder_hidden_size=128):
+    def __init__(self, hidden_size):
         super().__init__()
-        self.emb_c = nn.Conv1d(1024, encoder_hidden_size, 1)
-        self.emb_c_f0 = nn.Conv1d(1024, encoder_hidden_size, 1)
-        self.emb_f0 = nn.Conv1d(1, encoder_hidden_size, kernel_size=9, stride=4, padding=4) 
-        self.emb_norm_f0 = nn.Conv1d(1, encoder_hidden_size, 1)
+        self.emb_c = nn.Conv1d(1024, hidden_size, 1)
+        self.emb_c_f0 = nn.Conv1d(1024, hidden_size, 1)
+        self.emb_f0 = nn.Conv1d(1, hidden_size, kernel_size=9, stride=4, padding=4) 
+        self.emb_norm_f0 = nn.Conv1d(1, hidden_size, 1)
         self.emb_g = StyleEncoder(in_dim=80, hidden_dim=256, out_dim=256)
         
-        self.mel_enc_c = Encoder(encoder_hidden_size, encoder_hidden_size, 5, 1, 8, 80, gin_channels=256, p_dropout=0.1)
-        self.mel_enc_f = Encoder(encoder_hidden_size, encoder_hidden_size, 5, 1, 8, 80, gin_channels=256, p_dropout=0.1)
-        self.f0_enc = Encoder(encoder_hidden_size, encoder_hidden_size, 5, 1, 8, 128, gin_channels=256, p_dropout=0.1)
-        self.proj = nn.Conv1d(encoder_hidden_size, 1, 1)
+        self.mel_enc_c = Encoder(hidden_size, hidden_size, 5, 1, 8, 80, gin_channels=256, p_dropout=0)
+        self.mel_enc_f = Encoder(hidden_size, hidden_size, 5, 1, 8, 80, gin_channels=256, p_dropout=0)
+        self.f0_enc = Encoder(hidden_size, hidden_size, 5, 1, 8, 128, gin_channels=256, p_dropout=0)
+        self.proj = nn.Conv1d(hidden_size, 1, 1)
 
-    def forward(self, w2v, norm_f0, f0, x_mel, x_mask, f0_mask):
+    def forward(self, x_mel, w2v, norm_f0, f0, x_mask, f0_mask):
         content = self.emb_c(w2v) 
         content_f = self.emb_c_f0(w2v)
         f0 = self.emb_f0(f0)  
@@ -131,10 +130,30 @@ class DiffHierVC(BaseModule):
         self.beta_min = beta_min
         self.beta_max = beta_max
 
-        self.encoder = SynthesizerTrn(hps.model.encoder_hidden_size)
+        self.encoder = SynthesizerTrn(hps.model.hidden_size)
         self.f0_dec = F0_Diffusion(n_feats, 64, spk_dim, beta_min, beta_max)  
-        self.mel_dec = Mel_Diffusion(n_feats, dec_dim, spk_dim, beta_min, beta_max) 
-    
+        self.mel_dec = Mel_Diffusion(n_feats, dec_dim, spk_dim, beta_min, beta_max)  
+
+    @torch.no_grad()
+    def forward(self, x, w2v, norm_y_f0, f0_x, x_length, n_timesteps, mode='ml'):
+        x_mask = sequence_mask(x_length, x.size(2)).unsqueeze(1).to(x.dtype) 
+        f0_mask = sequence_mask(x_length*4, x.size(2)*4).unsqueeze(1).to(x.dtype)
+
+        max_length = int(x_length.max())
+        spk, y_mel, h_f0, y_f0_hat = self.encoder(x, w2v, norm_y_f0, f0_x, x_mask, f0_mask)  
+        f0_mean_x = self.f0_dec.compute_diffused_z_pr(f0_x, f0_mask, y_f0_hat, 1.0)
+       
+        z_f0 = f0_mean_x * f0_mask
+        z_f0 += torch.randn_like(z_f0, device=z_f0.device) 
+        o_f0 = self.f0_dec.reverse(z_f0, f0_mask, y_f0_hat*f0_mask, h_f0*f0_mask, spk, n_timesteps)
+        
+        z_mel = self.mel_dec.compute_diffused_z_pr(x, x_mask, y_mel, 1.0) 
+        z_mel += torch.randn_like(z_mel, device=z_mel.device)
+
+        o_mel = self.mel_dec.reverse(z_mel, x_mask, y_mel, spk, n_timesteps)
+   
+        return y_f0_hat, y_mel, o_f0, o_mel[:, :, :max_length]
+
     def infer_vc(self, x, x_w2v, x_f0_norm, x_f0, x_length, y, y_length, diffpitch_ts, diffvoice_ts):
         x_mask = sequence_mask(x_length, x.size(2)).unsqueeze(1).to(x.dtype)
         y_mask = sequence_mask(y_length, y.size(2)).unsqueeze(1).to(y.dtype)
@@ -146,12 +165,30 @@ class DiffHierVC(BaseModule):
         z_f0 = self.f0_dec.compute_diffused_z_pr(x_f0, f0_mask, y_f0_hat, 1.0) 
         z_f0 += torch.randn_like(z_f0, device=z_f0.device)
         pred_f0 = self.f0_dec.reverse(z_f0, f0_mask, y_f0_hat*f0_mask, enc_f0*f0_mask, spk, ts=diffpitch_ts)
-        
+        f0_zeros_mask = (x_f0 == 0).unsqueeze(1)
+        pred_f0[f0_zeros_mask.expand_as(pred_f0)] = 0 
+
         # Diff-Voice
         y_mel = self.encoder.mel_predictor(x_w2v, x_mask, spk, pred_f0)
         z_mel = self.mel_dec.compute_diffused_z_pr(x, x_mask, y_mel, 1.0) 
         z_mel += torch.randn_like(z_mel, device=z_mel.device) 
         o_mel = self.mel_dec.reverse(z_mel, x_mask, y_mel, spk, ts=diffvoice_ts)
       
-        return o_mel[:, :, :x_length]
+        return o_mel[:, :, :x_length]  
+
+
+    def compute_loss(self, x, w2v_x, norm_f0_x, f0_x, x_length): 
+        x_mask = sequence_mask(x_length, x.size(2)).unsqueeze(1).to(x.dtype)
+        f0_mask = sequence_mask(x_length*4, x.size(2)*4).unsqueeze(1).to(x.dtype)
+      
+        spk, y_mel, y_f0, y_f0_hat = self.encoder(x, w2v_x, norm_f0_x, f0_x, x_mask, f0_mask)  
+  
+        f0_loss = torch.sum(torch.abs(f0_x - y_f0_hat)*f0_mask) / (torch.sum(f0_mask)) 
+        mel_loss = torch.sum(torch.abs(x - y_mel)*x_mask) / (torch.sum(x_mask) * self.n_feats)
+        
+        f0_diff_loss = self.f0_dec.compute_t(f0_x, f0_mask, y_f0_hat, y_f0, spk)
+        mel_diff_loss, mel_recon_loss  = self.mel_dec.compute_t(x, x_mask, y_mel, spk)  
+
+        return mel_diff_loss, mel_recon_loss, f0_diff_loss, mel_loss, f0_loss
+
  
